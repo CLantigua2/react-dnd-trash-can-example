c381af3cb258cbde4111b6efc05f67be7058a423
Carlos Lantigua <clantigua954@gmail.com>
2019-01-14 09:42:45 -0500

    creating drag and drop

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import Item from "./Item"
+import Target from "./Target"
+import HTML5Backend from "react-dnd-html5-backend"
+import { DragDropContext } from "react-dnd"
+import "./App.css"
 
 class App extends Component {
+  state = {
+    items: [
+      { id: 1, name: "Item 1" },
+      { id: 2, name: "Item 2" },
+      { id: 3, name: "Item 3" },
+      { id: 4, name: "Item 4" }
+    ]
+  }
+
+  deleteItem = id => {
+    console.log("deleting id: " + id)
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="app-intro">
+          <div className="app-container">
+            {this.state.items.map((item, i) => (
+              <Item
+                key={item.id}
+                item={item}
+                handleDrop={id => this.deleteItem(id)}
+              />
+            ))}
+          </div>
+          <Target />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+// wrap your entire app in a context
+export default DragDropContext(HTML5Backend)(App)
---
+++ src/Item.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react"
+import { DragSource } from "react-dnd"
+
+// pass the dnd methods as props
+const itemSource = {
+  beginDrag(props) {
+    return props.item
+  },
+  endDrag(props, monitor, component) {
+    return props.handleDrop(props.item.id)
+  }
+}
+
+// create a collect object that holds the drag stuff
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  }
+}
+
+// wrap your dragable item in a connectDragSource so that your app knows it has the properties above
+class Item extends Component {
+  render() {
+    const { isDragging, connectDragSource, item } = this.props
+    const opacity = isDragging ? 0 : 1
+    return connectDragSource(
+      <div className="item" style={{ opacity }}>
+        <span>{item.name}</span>
+      </div>
+    )
+  }
+}
+
+export default DragSource("item", itemSource, collect)(Item)
